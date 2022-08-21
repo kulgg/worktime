a9794ab3467e2f015276f4fe0534f63654eb87d4
kulgg <75735874+kulgg@users.noreply.github.com>
2022-08-21 17:24:09 +0200

    Remove boilerplate

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,13 +2,11 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
 import { protectedExampleRouter } from "./protected-example-router";
 
 export const appRouter = createRouter()
-  .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("question.", protectedExampleRouter);
+	.transformer(superjson)
+	.merge("question.", protectedExampleRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
