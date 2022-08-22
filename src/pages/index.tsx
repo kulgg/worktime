a23ad2fa70019f3db6128215fc8529df3b33739d
kulgg <75735874+kulgg@users.noreply.github.com>
2022-08-22 19:17:36 +0200

    Implement mobile menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,8 @@ import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-
-type TechnologyCardProps = {
-	name: string;
-	description: string;
-	documentation: string;
-};
+import { MenuAlt2Icon, LoginIcon } from "@heroicons/react/solid";
+import { Menu } from "@headlessui/react";
 
 const Home: NextPage = () => {
 	const { data: session } = useSession();
@@ -21,18 +17,49 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-				<h1 className="text-4xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-					Work time tracker
-				</h1>
-				{session ? (
-					<div>
-						<span>{session.user?.email}</span>
-						<Link href="/api/auth/signout">SignOut</Link>
-					</div>
-				) : (
-					<Link href="/api/auth/signin">SignIn</Link>
-				)}
+			<main>
+				<nav className="py-2 px-3">
+					<Menu>
+						<div className="flex justify-between items-center">
+							<div className="mt-1">
+								<Menu.Button>
+									<MenuAlt2Icon className="h-7 w-7" />
+								</Menu.Button>
+							</div>
+							<h1 className="text-lg md:text-[5rem] leading-normal font-medium text-gray-700">
+								{session ? (
+									<div>
+										<span>{session.user?.name}</span>
+									</div>
+								) : (
+									<Link href="/api/auth/signin">Sign In👋</Link>
+								)}
+							</h1>
+						</div>
+						<Menu.Items className="grid grid-cols-1 justify-between fixed z-50 bg-white">
+							<Menu.Item>
+								<Link href="/">Blah</Link>
+							</Menu.Item>
+							<Menu.Item>
+								{({ active }) => (
+									<a
+										className={`${active && "bg-blue-500"}`}
+										href="/account-settings"
+									>
+										Documentation
+									</a>
+								)}
+							</Menu.Item>
+
+							{session && (
+								<Menu.Item>
+									<Link href="/api/auth/signout">Sign Out</Link>
+								</Menu.Item>
+							)}
+						</Menu.Items>
+					</Menu>
+				</nav>
+				<div>Hello</div>
 			</main>
 		</>
 	);
