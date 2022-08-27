7a109b5b09ea4c7ee0c55b80a61c10b232a9273c
kulgg <75735874+kulgg@users.noreply.github.com>
2022-08-27 11:12:26 +0200

    Restyle footer

--- src/components/mobilemenu.tsx
+++ src/components/mobilemenu.tsx
@@ -7,17 +7,17 @@ import Link from "next/link";
 
 const MobileMenu: React.FC<{}> = () => {
 	return (
-		<footer className="sticky z-50 bottom-0 w-full bg-grey-900 flex flex-col justify-center py-1 px-10 text-white">
+		<footer className="sticky z-50 bottom-0 w-full bg-grey-900 flex flex-col justify-center h-14 px-8 text-white">
 			<nav className="flex gap-8 justify-left">
 				<Link className="flex flex-col items-center gap-[2px] px-1" href="/">
-					<HomeIcon className="w-5 h-5" />
+					<HomeIcon className="w-4 h-4 text-grey-200" />
 					<span className="text-xs">Home</span>
 				</Link>
 				<Link
 					className="flex flex-col items-center gap-[2px] px-1"
 					href="/projects"
 				>
-					<BriefcaseIcon className="w-5 h-5" />
+					<BriefcaseIcon className="w-4 h-4 text-grey-200" />
 					<span className="text-xs">Projects</span>
 				</Link>
 			</nav>
