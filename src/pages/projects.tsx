a10747e309f94c6fd43cc9f2b9184de0946e0320
kulgg <75735874+kulgg@users.noreply.github.com>
2022-08-27 10:42:56 +0200

    Add session guard in projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,30 @@
 import { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import Header from "../components/header";
 import MobileMenu from "../components/mobilemenu";
 
 const Projects: NextPage = () => {
+	const { data: session } = useSession();
+
 	return (
 		<div className="flex flex-col min-h-screen text-white">
-			<div className="flex-grow">Hello</div>
+			<Header />
+			<div className="flex-grow">
+				{session ? (
+					<div>Projects</div>
+				) : (
+					<div className="text-center text-grey-200 text-lg mt-4">
+						<Link
+							href="/api/auth/signin"
+							className="text-blue-400 active:text-blue-300"
+						>
+							Sign In
+						</Link>{" "}
+						to view your projects
+					</div>
+				)}
+			</div>
 			<MobileMenu />
 		</div>
 	);
