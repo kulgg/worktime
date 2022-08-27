3fbb9db6afed81b9b9d9b101fcc0d26859dba14c
kulgg <75735874+kulgg@users.noreply.github.com>
2022-08-27 18:56:19 +0200

    Improve styling

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -80,59 +80,60 @@ const Projects: NextPage = () => {
 						<BriefcaseIcon className="w-5 h-5" />
 						<h2 className="text-lg">Projects</h2>
 					</div>
-					<div className="text-sm text-grey-100">
-						<label
-							htmlFor="small-toggle"
-							className="inline-flex relative items-center justify-end cursor-pointer"
-						>
-							<input
-								type="checkbox"
-								value=""
-								id="small-toggle"
-								className="sr-only peer"
-								onClick={() => setEditMode((y) => !y)}
-							/>
-							<div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-							<span className="ml-1 text-xs font-medium text-grey-200">
-								Edit Mode
-							</span>
-						</label>
-					</div>
+					<div className="text-sm text-grey-100"></div>
 				</div>
 				<div className="px-2 mt-4">
 					{session ? (
 						!isLoading &&
 						data && (
 							<div>
-								<div className="grid grid-cols-6 text-grey-200 text-xs">
-									<div className="col-span-4">Name</div>
-									<div className="col-span-1">Sessions</div>
-									<div className="flex justify-start items-center"></div>
-								</div>
-								{workPhases.map((x, i) => {
-									const backgroundColor =
-										i % 2 === 0 ? "bg-grey-500" : "bg-grey-600";
-									return (
-										<div
-											key={x.id}
-											className={`grid grid-cols-6 text-sm py-2 px-2 ${backgroundColor}`}
-										>
-											<div className="col-span-4">{x.name}</div>
-											<div className="col-span-1 text-center">
-												{x._count.workSessions}
-											</div>
-											{editMode && (
-												<div className="flex justify-end items-center text-red-400">
-													<TrashIcon
-														onClick={() => deleteWorkPhase({ id: x.id })}
-														className="w-4 h-4"
-													/>
-												</div>
-											)}
-										</div>
-									);
-								})}
 								<div>
+									<label
+										htmlFor="small-toggle"
+										className="inline-flex relative items-center justify-end cursor-pointer"
+									>
+										<input
+											type="checkbox"
+											value=""
+											id="small-toggle"
+											className="sr-only peer"
+											onClick={() => setEditMode((y) => !y)}
+										/>
+										<div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+										<span className="ml-1 text-xs font-medium text-grey-200">
+											Edit Mode
+										</span>
+									</label>
+									<div className="grid grid-cols-6 text-grey-200 text-xs">
+										<div className="col-span-4">Name</div>
+										<div className="col-span-1">Sessions</div>
+										<div className="flex justify-start items-center"></div>
+									</div>
+									{workPhases.map((x, i) => {
+										const backgroundColor =
+											i % 2 === 0 ? "bg-grey-500" : "bg-grey-600";
+										return (
+											<div
+												key={x.id}
+												className={`grid grid-cols-6 text-sm py-2 px-2 ${backgroundColor}`}
+											>
+												<div className="col-span-4">{x.name}</div>
+												<div className="col-span-1 text-center">
+													{x._count.workSessions}
+												</div>
+												{editMode && (
+													<div className="flex justify-end items-center text-red-400">
+														<TrashIcon
+															onClick={() => deleteWorkPhase({ id: x.id })}
+															className="w-4 h-4"
+														/>
+													</div>
+												)}
+											</div>
+										);
+									})}
+								</div>
+								<div className="relative inset-x-0 bottom-0">
 									{!createWorkPhaseIsLoading && editMode && (
 										<div>
 											<form
