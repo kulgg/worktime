44bfcf338d04ee111bf2b5e9744253d81952a722
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-09 19:23:05 +0200

    Improve signed out landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ const HomeContents = (): JSX.Element => {
 	if (!session) {
 		return (
 			<div>
-				<div className="mt-24 px-2 text-center flex flex-col justify-start items-center gap-5">
-					<h1 className="font-sans font-base text-2xl">
+				<div className="mt-24 px-2 sm:px-20 text-center flex flex-col justify-start items-center gap-5">
+					<h1 className="font-sans font-base text-2xl sm:text-3xl lg:text-4xl">
 						Start tracking your work time today. For free.
 					</h1>
 					<GoogleButton onClick={() => signIn("google")} />
