47f9bf0383032a7ec9a20842fc5d24afc0b85b08
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-10 11:09:46 +0200

    Add loading animation to projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -15,45 +15,30 @@ import {
 } from "../shared/work-session-validator";
 import { trpc } from "../utils/trpc";
 import { authOptions } from "./api/auth/[...nextauth]";
+import LoadingSVG from "../assets/puff.svg";
+import Image from "next/image";
 
-const Projects = () => {
-	const { data: session } = useSession();
-
-	const workPhaseWithSessionCounts = Prisma.validator<Prisma.WorkPhaseArgs>()({
-		include: {
-			_count: {
-				select: {
-					workSessions: true,
-				},
+const workPhaseWithSessionCounts = Prisma.validator<Prisma.WorkPhaseArgs>()({
+	include: {
+		_count: {
+			select: {
+				workSessions: true,
 			},
 		},
-	});
-	type WorkPhaseWithSessionCounts = Prisma.WorkPhaseGetPayload<
-		typeof workPhaseWithSessionCounts
-	>;
+	},
+});
+type WorkPhaseWithSessionCounts = Prisma.WorkPhaseGetPayload<
+	typeof workPhaseWithSessionCounts
+>;
 
+const ProjectList = ({
+	workPhases,
+}: {
+	workPhases: WorkPhaseWithSessionCounts[];
+}): JSX.Element => {
 	const [editMode, setEditMode] = useState<boolean>(false);
 
-	const { data: workPhases, isLoading } = trpc.useQuery([
-		"workphases.get-all-with-session-counts",
-	]);
-
 	const qc = useQueryClient();
-
-	const { mutate: createWorkPhase, isLoading: createWorkPhaseIsLoading } =
-		trpc.useMutation(["workphases.create"], {
-			onSuccess: (createdWorkPhase: WorkPhaseWithSessionCounts) => {
-				qc.setQueryData(
-					["workphases.get-all-with-session-counts"],
-					(old: WorkPhaseWithSessionCounts[] | undefined) => {
-						if (!old) {
-							return [createdWorkPhase];
-						}
-						return [...old, createdWorkPhase];
-					}
-				);
-			},
-		});
 
 	const { mutate: deleteWorkPhase, isLoading: deleteWorkPhaseIsLoading } =
 		trpc.useMutation(["workphases.delete"], {
@@ -69,6 +54,73 @@ const Projects = () => {
 				);
 			},
 		});
+
+	return (
+		<div>
+			<label
+				htmlFor="small-toggle"
+				className="inline-flex relative items-center justify-end cursor-pointer"
+			>
+				<input
+					type="checkbox"
+					value=""
+					id="small-toggle"
+					className="sr-only peer"
+					onClick={() => setEditMode((y) => !y)}
+				/>
+				<div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+				<span className="ml-1 text-xs font-medium text-grey-200">
+					Edit Mode
+				</span>
+			</label>
+			<div className="grid grid-cols-6 text-grey-200 text-xs">
+				<div className="col-span-4">Name</div>
+				<div className="col-span-1">Sessions</div>
+			</div>
+			{workPhases.map((x, i) => {
+				const backgroundColor = i % 2 === 0 ? "bg-grey-500" : "bg-grey-600";
+				return (
+					<div
+						key={x.id}
+						className={`grid grid-cols-6 text-sm py-2 px-2 ${backgroundColor}`}
+					>
+						<div className="col-span-4">{x.name}</div>
+						<div className="col-span-1 px-2">{x._count.workSessions}</div>
+						{editMode && (
+							<div className="flex justify-end items-center text-red-400">
+								<TrashIcon
+									onClick={() => deleteWorkPhase({ id: x.id })}
+									className="w-4 h-4 cursor-pointer hover:text-red-500"
+								/>
+							</div>
+						)}
+					</div>
+				);
+			})}
+		</div>
+	);
+};
+
+const Projects = () => {
+	const { data: workPhases, isLoading } = trpc.useQuery([
+		"workphases.get-all-with-session-counts",
+	]);
+
+	const qc = useQueryClient();
+
+	const { mutate: createWorkPhase } = trpc.useMutation(["workphases.create"], {
+		onSuccess: (createdWorkPhase: WorkPhaseWithSessionCounts) => {
+			qc.setQueryData(
+				["workphases.get-all-with-session-counts"],
+				(old: WorkPhaseWithSessionCounts[] | undefined) => {
+					if (!old) {
+						return [createdWorkPhase];
+					}
+					return [...old, createdWorkPhase];
+				}
+			);
+		},
+	});
 
 	const {
 		register,
@@ -93,85 +145,48 @@ const Projects = () => {
 					<div className="text-sm text-grey-100"></div>
 				</div>
 				<div className="mt-4">
-					{session && !isLoading && workPhases && (
-						<div>
-							<div>
-								<label
-									htmlFor="small-toggle"
-									className="inline-flex relative items-center justify-end cursor-pointer"
-								>
-									<input
-										type="checkbox"
-										value=""
-										id="small-toggle"
-										className="sr-only peer"
-										onClick={() => setEditMode((y) => !y)}
-									/>
-									<div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-									<span className="ml-1 text-xs font-medium text-grey-200">
-										Edit Mode
-									</span>
-								</label>
-								<div className="grid grid-cols-6 text-grey-200 text-xs">
-									<div className="col-span-4">Name</div>
-									<div className="col-span-1">Sessions</div>
-								</div>
-								{workPhases.map((x, i) => {
-									const backgroundColor =
-										i % 2 === 0 ? "bg-grey-500" : "bg-grey-600";
-									return (
-										<div
-											key={x.id}
-											className={`grid grid-cols-6 text-sm py-2 px-2 ${backgroundColor}`}
-										>
-											<div className="col-span-4">{x.name}</div>
-											<div className="col-span-1 px-2">
-												{x._count.workSessions}
-											</div>
-											{editMode && (
-												<div className="flex justify-end items-center text-red-400">
-													<TrashIcon
-														onClick={() => deleteWorkPhase({ id: x.id })}
-														className="w-4 h-4 cursor-pointer hover:text-red-500"
-													/>
-												</div>
-											)}
-										</div>
-									);
-								})}
+					<div>
+						{isLoading ? (
+							<div className="flex animate-fade-in-delay justify-center mt-12">
+								<Image
+									src={LoadingSVG}
+									alt="loading..."
+									width={50}
+									height={50}
+								/>
 							</div>
-							<div className="flex items-center justify-center">
-								<div className="py-4 w-full bg-grey-600">
-									{!createWorkPhaseIsLoading && (
-										<div>
-											<form
-												onSubmit={handleSubmit((data) => {
-													createWorkPhase(data);
-												})}
-												className="flex items-center gap-2"
+						) : (
+							workPhases && <ProjectList workPhases={workPhases} />
+						)}
+						<div className="flex items-center justify-center">
+							<div className="py-4 w-full bg-grey-600">
+								<div>
+									<form
+										onSubmit={handleSubmit((data) => {
+											createWorkPhase(data);
+										})}
+										className="flex items-center gap-2"
+									>
+										<input
+											{...register("name")}
+											className="input w-full rounded-xl h-9 bg-grey-700 text-grey-100 text-sm"
+											defaultValue={0}
+											type="text"
+											placeholder="Secret Project"
+										/>
+										<div className="">
+											<button
+												type="submit"
+												className="w-24 h-8 bg-blue-500 rounded-md text-sm"
 											>
-												<input
-													{...register("name")}
-													className="input w-full rounded-xl h-9 bg-grey-700 text-grey-100 text-sm"
-													defaultValue={0}
-													type="text"
-													placeholder="Secret Project"
-												/>
-												<div className="">
-													<button
-														type="submit"
-														className="w-24 h-8 bg-blue-500 rounded-md text-sm"
-													>
-														Add
-													</button>
-												</div>
-											</form>
+												Add
+											</button>
 										</div>
-									)}
+									</form>
 								</div>
 							</div>
 						</div>
-					)}
+					</div>
 				</div>
 			</main>
 		</PageContainer>
