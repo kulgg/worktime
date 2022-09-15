37f7b21c4748d4e2c95cb48765b5baba9e266736
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-15 20:15:09 +0200

    Remove title from _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
-				<title>Work Time Tracker</title>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link
